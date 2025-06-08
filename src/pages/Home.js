5cfba18dc5be8861908dbb0491f0481510e13736
Ingenieur25 <harounmouhamed25@gmail.com>
2025-06-08 13:36:59 +0100

    Update Home page description

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import './Home.css'; // Assurez-vous d'importer le fichier CSS
 function Home() {
     return (
         <div className="home-container">
-            <h1 className="home-title">Welcome to the Mauritania Economic Indicator Prediction Application.</h1>
+            <h1 className="home-title">Welcome to the Mauritania Economic and Demographic Indicator Prediction Application.</h1>
             <p className="home-description">
             This web application uses advanced machine learning models to enable you to predict economic indicators (GDP) and demographic data (life expectancy, population) of Mauritania for the coming years. It simplifies the prediction process based on historical data and allows you to explore future trends through a user-friendly interface.
             </p>
